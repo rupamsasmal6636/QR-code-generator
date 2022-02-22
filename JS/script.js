e23f7d2e6866d8d10df04b6667275a95ba80df61
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2022-02-22 21:48:01 +0530

    fix qr generator bug and add responsiveness for mobile

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,23 @@
-const qrcode=document.getElementById('image');
+const qrcode=document.querySelector('img');
 const qrtext= document.getElementById('txt');
 const qrGenbtn= document.getElementById('btnGenerate'); 
 const qrDownbtn=document.getElementById('btnDownload');
 
 qrGenbtn.addEventListener('click', ()=>{
-    let size= "1000x1000";
+    let size= "750x750";
     let data= qrtext.value;
-    let baseURL="http://api.qrserver.com/v1/create-qr-code/"; //api call
+    let baseURL="https://api.qrserver.com/v1/create-qr-code/"; //api call
 
     let url=`${baseURL}?data=${data}&size=${size}`;
-    
+    console.log(data);
     qrcode.src=url;
+    qrtext.value="";
 });
 
-qrDownbtn.addEventListener('click', ()=>{
-    // alert('download start');
-    let imgPath=qrcode.src;
-    let fileName= "image.jpeg";
+// qrDownbtn.addEventListener('click', ()=>{
+//     // alert('download start');
+//     let imgPath=qrcode.src;
+//     let fileName= "image.jpeg";
 
-    saveAs(imgPath,fileName);
-});
+//     saveAs(imgPath,fileName);
+// });
